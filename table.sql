daea783cd04923175e9d308038fd5dc46b45d7d6
dshamilja <dshamilja.camenisch@stud.fhgr.ch>
2023-10-03 14:11:28 +0200

    table

--- table.sql
+++ table.sql
@@ -4,7 +4,13 @@ CREATE TABLE person (
     username VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    basic_premium DECIMAL(10, 2),
+    grund_pramie DECIMAL(10, 2),
     password VARCHAR(255) NOT NULL,
     ahv_number VARCHAR(20) UNIQUE
 );
+
+-- Insert person
+INSERT INTO person (person_id, username, first_name, last_name, grund_pramie, ahv_number)
+VALUES 
+('092394', 'Maxmuster', 'Max', 'Muster', 'CHF 500.00', '798.304.393'),
+('545665', 'Lisameier', 'Lisa', 'Meier', 'CHF 700.00', '768.304.323'),
